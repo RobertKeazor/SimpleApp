2090ca2540bed4f5cf70d125593b85f86574bd2a
Robert Keazor <rkeazor@gmail.com>
2019-03-09 23:48:50 -0500

    Refactor LoginScreen and setup Firebase registration

--- app/src/main/java/com/robertkeazor/chatapp/ui/MainActivity.kt
+++ app/src/main/java/com/robertkeazor/chatapp/ui/MainActivity.kt
@@ -2,9 +2,11 @@ package com.robertkeazor.chatapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import com.robertkeazor.chatapp.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/robertkeazor/chatapp/ui/main/login/LoginViewModel.kt
+++ app/src/main/java/com/robertkeazor/chatapp/ui/main/login/LoginViewModel.kt
@@ -1,6 +1,6 @@
 package com.robertkeazor.chatapp.ui.main.login
 
-import android.util.Log
+
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.robertkeazor.chatapp.manager.FirebaseManager
@@ -11,18 +11,26 @@ import javax.inject.Inject
 class LoginViewModel @Inject constructor(val firebaseManager: FirebaseManager) : ViewModel() {
     val userName = MutableLiveData<String>()
     val password = MutableLiveData<String>()
+    val email = MutableLiveData<String>()
     val registerError = MutableLiveData<String>()
     val disposables = CompositeDisposable()
 
     fun onRegisterButtonClick() {
-        disposables.add(
-            firebaseManager.registerUser(userName.value!!, password.value!!)
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnComplete { if (registerError.value.isNullOrEmpty()) registerError.value = null }
-                .doOnError { registerError.value = it.localizedMessage }
-                .onErrorComplete()
-                .subscribe()
-        )
+        if (hasValidFields(userName.value, password.value)) {
+            disposables.add(
+                firebaseManager.registerUser(userName.value!!, password.value!!)
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnComplete { if (registerError.value.isNullOrEmpty()) registerError.value = null }
+                    .doOnError { registerError.value = it.localizedMessage }
+                    .onErrorComplete()
+                    .subscribe()
+            )
+        }
+    }
+
+    fun hasValidFields(vararg fieldValues: String?): Boolean {
+        fieldValues.forEach { if (it.isNullOrEmpty()) return false }
+        return true
     }
 
     override fun onCleared() {
